6dcdb86c6ce4bed1cffa0ecee6c2d8dbd99fd16c
exxinor <exxinor@yahoo.de>
2014-05-08 18:11:14 +0200

    adding the first classes

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+
+
+class ScreenManager():
+    pass
+
+
+class MathFunKidsApp(App):
+    def build(self):
+        pass
+    
+if __name__ == "__main__":
+    MathFunKidsApp().run()
