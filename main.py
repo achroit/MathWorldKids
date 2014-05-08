181f44b9c0e2590d27b926ac3bd808f96df731e4
exxinor <exxinor@yahoo.de>
2014-05-08 19:11:37 +0200

    changing classes to fit the main app layout

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class ScreenManager():
+# Declare both screens
+class MenuScreen(Screen):
     pass
 
 
-class MathFunKidsApp(App):
+class StartScreen(Screen):
+    pass
+
+
+class ProgressScreen(Screen):
+    pass
+
+
+class HelpScreen(Screen):
+    pass
+
+
+class AboutScreen(Screen):
+    pass
+
+
+class MathApp(App):
+
     def build(self):
-        pass
-    
-if __name__ == "__main__":
-    MathFunKidsApp().run()
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(StartScreen(name='settings'))
+        sm.add_widget(ProgressScreen(name='progress'))
+        sm.add_widget(HelpScreen(name='help'))
+        sm.add_widget(AboutScreen(name='help'))
+        return sm
+
+if __name__ == '__main__':
+    MathApp().run()
