0dc5980c48cdce8bcd5fa001fda875a6d54a8ad6
exxinor <exxinor@yahoo.de>
2014-05-11 20:11:00 +0200

    taskCreator generates random answers

--- taskCreator.py
+++ taskCreator.py
@@ -2,37 +2,81 @@ import random
 
 
 def add_task(difficulty=1):
-    c = []
+    operands = []
+    whole_task = []
     if difficulty == 1:
-        a = random.randint(0, 5)
-        b = random.randint(0, 5)
+        operand_1 = random.randint(0, 5)
+        operand_2 = random.randint(0, 5)
+        option_1 = operand_1 + operand_2
+        option_2 = random.randint(0, 5) + random.randint(0, 5)
+        option_3 = random.randint(0, 5) + random.randint(0, 5)
+        option_4 = random.randint(0, 5) + random.randint(0, 5)
     elif difficulty == 2:
-        a = random.randint(0, 10)
-        b = random.randint(0, 10)
+        operand_1 = random.randint(0, 10)
+        operand_2 = random.randint(0, 10)
+        option_1 = operand_1 + operand_2
+        option_2 = random.randint(0, 10) + random.randint(0, 10)
+        option_3 = random.randint(0, 10) + random.randint(0, 10)
+        option_4 = random.randint(0, 10) + random.randint(0, 10)
     elif difficulty == 3:
-        a = random.randint(0, 15)
-        b = random.randint(0, 10)
+        operand_1 = random.randint(0, 15)
+        operand_2 = random.randint(0, 10)
+        option_1 = operand_1 + operand_2
+        option_2 = random.randint(0, 15) + random.randint(0, 10)
+        option_3 = random.randint(0, 15) + random.randint(0, 10)
+        option_4 = random.randint(0, 15) + random.randint(0, 10)
     elif difficulty == 4:
-        a = random.randint(0, 20)
-        b = random.randint(0, 15)
+        operand_1 = random.randint(0, 20)
+        operand_2 = random.randint(0, 15)
+        option_1 = operand_1 + operand_2
+        option_2 = random.randint(0, 20) + random.randint(0, 15)
+        option_3 = random.randint(0, 20) + random.randint(0, 15)
+        option_4 = random.randint(0, 20) + random.randint(0, 15)
     elif difficulty == 5:
-        a = random.randint(0, 25)
-        b = random.randint(0, 20)
+        operand_1 = random.randint(0, 25)
+        operand_2 = random.randint(0, 20)
+        option_1 = operand_1 + operand_2
+        option_2 = random.randint(0, 25) + random.randint(0, 20)
+        option_3 = random.randint(0, 25) + random.randint(0, 20)
+        option_4 = random.randint(0, 25) + random.randint(0, 20)
     elif difficulty == 6:
-        a = random.randint(0, 50)
-        b = random.randint(0, 15)
+        operand_1 = random.randint(0, 50)
+        operand_2 = random.randint(0, 15)
+        option_1 = operand_1 + operand_2
+        option_2 = random.randint(0, 50) + random.randint(0, 15)
+        option_3 = random.randint(0, 50) + random.randint(0, 15)
+        option_4 = random.randint(0, 50) + random.randint(0, 15)
     elif difficulty == 7:
-        a = random.randint(0, 75)
-        b = random.randint(0, 25)
+        operand_1 = random.randint(0, 75)
+        operand_2 = random.randint(0, 25)
+        option_1 = operand_1 + operand_2
+        option_2 = random.randint(0, 75) + random.randint(0, 25)
+        option_3 = random.randint(0, 75) + random.randint(0, 25)
+        option_4 = random.randint(0, 75) + random.randint(0, 25)
     elif difficulty == 8:
-        a = random.randint(0, 100)
-        b = random.randint(0, 50)
+        operand_1 = random.randint(0, 100)
+        operand_2 = random.randint(0, 50)
+        option_1 = operand_1 + operand_2
+        option_2 = random.randint(0, 100) + random.randint(0, 50)
+        option_3 = random.randint(0, 100) + random.randint(0, 50)
+        option_4 = random.randint(0, 100) + random.randint(0, 50)
     elif difficulty == 9:
-        a = random.randint(0, 500)
-        b = random.randint(0, 500)
+        operand_1 = random.randint(0, 500)
+        operand_2 = random.randint(0, 500)
+        option_1 = operand_1 + operand_2
+        option_2 = random.randint(0, 500) + random.randint(0, 500)
+        option_3 = random.randint(0, 500) + random.randint(0, 500)
+        option_4 = random.randint(0, 500) + random.randint(0, 500)
     elif difficulty >= 10:
-        a = random.randint(0, 1000)
-        b = random.randint(0, 1000)
-    c = [a, b]
-    random.shuffle(c)
-    return c
+        operand_1 = random.randint(0, 1000)
+        operand_2 = random.randint(0, 1000)
+        option_1 = operand_1 + operand_2
+        option_2 = random.randint(0, 1000) + random.randint(0, 1000)
+        option_3 = random.randint(0, 1000) + random.randint(0, 1000)
+        option_4 = random.randint(0, 1000) + random.randint(0, 1000)
+    operands = [operand_1, operand_2]
+    answers = [option_1, option_2, option_3, option_4]
+    random.shuffle(operands)
+    random.shuffle(answers)
+    whole_task = operands + answers
+    return whole_task
