24aa4960a3bbbc3f15a3501826e463982f43126c
exxinor <exxinor@yahoo.de>
2014-05-11 18:20:18 +0200

    adjusted range of random ints to be generated, shuffled the two values to get more variance

--- taskCreator.py
+++ taskCreator.py
@@ -2,7 +2,7 @@ import random
 
 
 def add_task(difficulty=1):
-    a = b = 0
+    c = []
     if difficulty == 1:
         a = random.randint(0, 5)
         b = random.randint(0, 5)
@@ -11,26 +11,28 @@ def add_task(difficulty=1):
         b = random.randint(0, 10)
     elif difficulty == 3:
         a = random.randint(0, 15)
-        b = random.randint(0, 15)
+        b = random.randint(0, 10)
     elif difficulty == 4:
         a = random.randint(0, 20)
-        b = random.randint(0, 20)
+        b = random.randint(0, 15)
     elif difficulty == 5:
         a = random.randint(0, 25)
-        b = random.randint(0, 25)
+        b = random.randint(0, 20)
     elif difficulty == 6:
         a = random.randint(0, 50)
         b = random.randint(0, 15)
     elif difficulty == 7:
         a = random.randint(0, 75)
-        b = random.randint(0, 75)
+        b = random.randint(0, 25)
     elif difficulty == 8:
         a = random.randint(0, 100)
-        b = random.randint(0, 100)
+        b = random.randint(0, 50)
     elif difficulty == 9:
         a = random.randint(0, 500)
         b = random.randint(0, 500)
     elif difficulty >= 10:
         a = random.randint(0, 1000)
         b = random.randint(0, 1000)
-    return a, b
+    c = [a, b]
+    random.shuffle(c)
+    return c
